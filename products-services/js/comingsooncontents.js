9017fa5fe261265ca3bf49a007f908f7bde7fc9d
Sophia <sodavis10@gmail.com>
2016-12-19 13:59:53 -1000

    Update, make some of the pages work

---
+++ products-services/js/comingsooncontents.js
@@ -0,0 +1,55 @@
+var content1=
+('<br><h3>EXPERIMENTAL PRODUCT</h3><br>')+
+('<div class="contentlandinggriddescription">THIS PRODUCT IS REALLY EXPERIMENTAL </div>')+
+('<div class="contentlandinggriddescription">')+
+('Experimenting With Experimental-ness<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content2=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content3=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content4=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content5=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content6=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content7=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content8=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
---
+++ products-services/js/contents.js
@@ -0,0 +1,55 @@
+var content1=
+('<br><h3>TIGERSHARK TAREA</h3><br>')+
+('<div class="contentlandinggriddescription">REVOLUTIONIZING THE WAY KCS STUDENTS CHECK HOMEWORK </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content2=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content3=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content4=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content5=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content6=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content7=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
+
+var content8=
+('<br><h3>TITLE</h3><br>')+
+('<div class="contentlandinggriddescription">SHORT DESCRIPTION </div>')+
+('<div class="contentlandinggriddescription">')+
+('Longer Description<br></div>')+
+('<div class="contentlandinggriddescription"><a href="#woah"><i>GO TO WEBSITE</i></a></div>');
